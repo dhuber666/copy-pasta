cea65cd40e218d5538144b7352e8823899e0247d
dhuber666 <dhuber666@gmail.com>
2020-07-18 15:52:27 +0200

    fixed absolute url bug - hopefully :)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/index.css";
 import { Provider } from "next-auth/client";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.SITE || "http://localhost:3000";
 
 function MyApp({ Component, pageProps }) {
   const { session } = pageProps;
